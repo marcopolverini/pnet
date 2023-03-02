c0a757bc543dcbf58faf3b0f70f4354a2eb5fe42
marcopolverini <36706597+marcopolverini@users.noreply.github.com>
2023-03-02 15:30:00 +0100

    Add files via upload

---
+++ NETCONF_YANG_hands_on/netconf/example2.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+"""
+Copyright (c) 2019 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.0 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+
+__author__ = "Hank Preston <hapresto@cisco.com>"
+__contributors__ = "Bryan Byrne <brybyrne@cisco.com>"
+__copyright__ = "Copyright (c) 2019 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.0"
+
+
+from device_info import ios_xe1
+from ncclient import manager
+import xmltodict
+
+# NETCONF filter to use
+netconf_filter = open("filter-ietf-interfaces.xml").read()
+#netconf_filter = open("filtro_prova.xml").read()
+
+if __name__ == '__main__':
+    with manager.connect(host=ios_xe1["address"],
+                         port=ios_xe1["port"],
+                         username=ios_xe1["username"],
+                         password=ios_xe1["password"],
+                         hostkey_verify=False) as m:
+
+        # Get Configuration and State Info for Interface
+        netconf_reply = m.get(netconf_filter)
+
+        # Process the XML and store in useful dictionaries
+        intf_details = xmltodict.parse(netconf_reply.xml)["rpc-reply"]["data"]
+        intf_config = intf_details["interfaces"]["interface"]
+        intf_info = intf_details["interfaces-state"]["interface"]
+
+        print("")
+        print("Interface Details:")
+        print("  Name: {}".format(intf_config["name"]))
+        print("  Description: {}".format(intf_config["description"]))
+        print("  Type: {}".format(intf_config["type"]["#text"]))
+        print("  MAC Address: {}".format(intf_info["phys-address"]))
+        print("  Packets Input: {}".format(intf_info["statistics"]["in-unicast-pkts"]))
+        print("  Packets Output: {}".format(intf_info["statistics"]["out-unicast-pkts"]))
